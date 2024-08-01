29c36bcf6fd7cd643f84bd87b19ced5da4a4772c
sebastianposda121 <sebaspj67@gmail.com>
2024-07-31 23:06:56 -0500

    first commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+
+import { HeroService } from '../hero.service';
+import {Hero} from "../heroe";
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.scss' ]
+})
+export class HeroDetailComponent implements OnInit {
+  hero!: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +(this.route.snapshot.paramMap.get('id') || 0);
+    this.heroService.getHero(id)
+      .subscribe(hero => {
+        if(hero){
+          this.hero = hero;
+        }
+      });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+}
